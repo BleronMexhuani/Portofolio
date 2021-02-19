13154b52ad206d41d0bf7ac7ce686ef7d273c1b8
BleronMexhuani <58087682+BleronMexhuani@users.noreply.github.com>
2021-02-19 12:00:52 +0100

    Create login.php

---
+++ login.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="login.css">
+    <title>Login</title>
+</head>
+
+<body>
+    
+    <div class="container">
+        <div class="login-box">
+        <div class="row">
+            <div class="col-md-6 login-left">
+                <h2>Login Here</h2>
+                <form action="validation.php" method="POST">
+                    <div class="form-group">
+                        <label>Username</label>
+                        <input type="text" name="user" class="form-control" required>
+                    </div>
+                    <div class="form-group">
+                        <label>Password</label>
+                        <input type="password" name="password" class="form-control" required>
+                    </div>
+                    <button type="submit" class="btn btn-primary"> Login </button>
+                </form>
+            </div>
+            <div class="col-md-6 login-right">
+                <h2>Register Here</h2>
+                <form action="registration.php" method="POST">
+                    <div class="form-group">
+                        <label>Username</label>
+                        <input type="text" name="user" class="form-control" required>
+                    </div>
+                    <div class="form-group">
+                        <label>Password</label>
+                        <input type="password" name="password" class="form-control" required>
+                    </div>
+                    <button type="submit" class="btn btn-primary"> Register </button>
+            </div>
+        </div>
+        </div>
+
+</body>
+
+</html>
